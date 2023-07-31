ccc5daf64bc36d41320e439db244410aa19a722b
Avik Ghosh <36378913+ATinyLearner@users.noreply.github.com>
2023-07-31 10:01:16 +0530

    1st session added

---
+++ Python MYP 3 31_07_23.py
@@ -0,0 +1,37 @@
+# print helps us to show text or number in console
+print("I am a student.")
+print(3.23)
+
+# data types
+int #  integer 1,2,3
+float # float 1.23,2.34
+str #string is text
+
+
+var = "myp3" # variable is a bucket where we put a data (string, number)
+print(type(var))
+
+# python is a loosely typed language
+print("12.3")
+print(type("12.3"))
+print(type(12.3))
+
+# binary data types (discuss later)
+
+# condional statements
+# ramu wants to have a ps5
+marks_ramu=40
+if(marks_ramu>90):
+    print("Ramu will get a ps5")
+elif(marks_ramu<90 and marks_ramu>70):
+    print("Ramu will be gifted with a samosa and chutni")
+elif(marks_ramu<70 and marks_ramu>50):
+    print("Ramu will be eating pizza with only pinapples")
+else:
+    print("Ramu will be restricted from using electronic devices")
+
+
+
+
+
+

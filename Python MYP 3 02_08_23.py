6fc0205bbf3585165904f65174b94fe4313538be
Avik Ghosh <36378913+ATinyLearner@users.noreply.github.com>
2023-08-02 10:13:05 +0530

    Python class 02.08.23 added

---
+++ Python MYP 3 02_08_23.py
@@ -0,0 +1,16 @@
+import math # to use the library you have import it like this
+
+# print(math.ceil(3.997))
+# print(math.pi)
+# print(math.floor(3.45))
+# print(math.fabs(4-13))
+# print(4-13)
+
+print(2*5) ## multiplication
+print(2**5) #exponential
+
+print(15//2)
+
+print(math.pow(2,3))
+
+
